4d647016df4d60cf68cb43b3b86289031ee4529a
RashLop <lopezmejiarasshid@outlook.com>
2026-04-02 22:15:55 -0400

    feat: Dto de usuarios actualizados con los nuevos atributos

--- Domain/DTOs/UsuarioRegistroDTO.cs
+++ Domain/DTOs/UsuarioRegistroDTO.cs
@@ -2,19 +2,16 @@ using System;
 
 namespace ProyectoArqSoft.DTO
 {
-    public class UsuarioActualizacionDto
+    public class UsuarioRegistroDto
     {
-        public int IdUsuario { get; set; }
         public string Nombres { get; set; } = string.Empty;
         public string ApellidoPaterno { get; set; } = string.Empty;
         public string ApellidoMaterno { get; set; } = string.Empty;
+        public string Ci { get; set; } = string.Empty;
+        public string CiExtencion { get; set; } = string.Empty;
         public string Telefono { get; set; } = string.Empty;
-        public sbyte Activo { get; set; } = 1;
-        public DateTime FechaRegistro { get; set; } = DateTime.Now;
-        public DateTime? UltimaActualizacion { get; set; }
         public string Email { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
-        public sbyte MustChangePassword { get; set; } = 1;
+        public string Password { get; set; } = string.Empty;
     }
 }
