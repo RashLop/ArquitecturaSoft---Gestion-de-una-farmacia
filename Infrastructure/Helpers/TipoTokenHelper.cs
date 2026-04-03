2e112e5d615bf2a481194e70233124962728db2d
RashLop <lopezmejiarasshid@outlook.com>
2026-04-03 14:55:29 -0400

    feat: ayuda de tipo de token agregado

---
+++ Infrastructure/Helpers/TipoTokenHelper.cs
@@ -0,0 +1,9 @@
+namespace ProyectoArqSoft.Helpers
+{
+    public static class TipoTokenConstantes
+    {
+        public const string ActivacionCuenta = "ACTIVACION_CUENTA";
+        public const string ResetPassword = "RESET_PASSWORD";
+        public const string ConfirmacionEmail = "CONFIRMACION_EMAIL";
+    }
+}
