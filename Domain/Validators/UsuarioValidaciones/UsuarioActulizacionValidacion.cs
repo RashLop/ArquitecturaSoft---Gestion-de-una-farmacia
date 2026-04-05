5098a4dd8367a4d8c810ad46614bcee34b3086bf
RashLop <lopezmejiarasshid@outlook.com>
2026-04-05 17:54:52 -0400

    fix: Usuario actulizacion rename para ir arreglar problemas

---
+++ Domain/Validators/UsuarioValidaciones/UsuarioActulizacionValidacion.cs
@@ -0,0 +1,18 @@
+using ProyectoArqSoft.DTO;
+
+namespace ProyectoArqSoft.Validaciones
+{
+    public class UsuarioActualizacionValidacion : UsuarioValidacionBase, IResult<UsuarioActualizacionDto>
+    {
+        public Result Validar(UsuarioActualizacionDto dto)
+        {
+            if (dto == null)
+                return Result.Fail("Datos nulos.");
+
+            if (dto.IdUsuario <= 0)
+                return Result.Fail("El identificador del usuario no es válido.");
+
+            return EmailValido(dto.Email) ?? Result.Ok();
+        }
+    }
+}
